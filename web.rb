69c468e9428b6e2862e558aaff2d43e48e33ad94
Adam Wiggins <adam@heroku.com>
2011-02-24 21:39:22 -0800

    1k / 16k / 64k

--- web.rb
+++ web.rb
@@ -3,6 +3,8 @@ require 'sinatra'
 require 'sinatra/cache_assets'
 use Sinatra::CacheAssets
 
-get '/' do
-  "hello"
+%w(1k 16k 64k).each do |size|
+  get "/dyno/#{size}" do
+    File.read("public/varnish/#{size}")
+  end
 end
