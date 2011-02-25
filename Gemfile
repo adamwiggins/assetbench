91ce2de1141cf41622e259033ba73362c4f3fb55
Adam Wiggins <adam@heroku.com>
2011-02-24 20:54:52 -0800

    cache assets

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source :gemcutter
 gem 'sinatra'
 gem 'thin'
+gem 'sinatra-cache-assets', '0.0.3'
--- web.rb
+++ web.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
 
+require 'sinatra/cache_assets'
+use Sinatra::CacheAssets
+
 get '/' do
   "hello"
 end
