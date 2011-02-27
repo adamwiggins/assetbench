cdb603f521ddd53664debe5e735d9f57efb4b354
Adam Wiggins <adam@heroku.com>
2011-02-27 10:19:14 -0800

    use dalli, rack-cache, and memcache to cache pages

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source :gemcutter
 gem 'sinatra'
 gem 'thin'
 gem 'sinatra-cache-assets', '0.0.3'
+gem 'rack-cache'
+gem 'dalli'
--- web.rb
+++ web.rb
@@ -3,8 +3,21 @@ require 'sinatra'
 require 'sinatra/cache_assets'
 use Sinatra::CacheAssets
 
+require 'dalli'
+CACHE = Dalli::Client.new
+
+require 'rack/cache'
+use Rack::Cache, :metastore => CACHE
+
 %w(1k 16k 64k).each do |size|
   get "/dyno/#{size}" do
+    File.read("public/varnish/#{size}")
+  end
+end
+
+%w(1k 16k 64k).each do |size|
+  get "/cached/#{size}" do
+		response['Cache-Control'] = "public, max-age=#{12*60*60}"
     File.read("public/varnish/#{size}")
   end
 end
